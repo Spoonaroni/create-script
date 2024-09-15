2cef0c7a5d206621b1db00afe8b9bbee1be4d351
Spoonaroni <163045653+Spoonaroni@users.noreply.github.com>
2024-09-14 21:51:10 -0400

    Create repo

---
+++ create.sh
@@ -0,0 +1,68 @@
+#! /bin/bash
+
+echo "0(Exercises) or 1(Projects)"
+read n
+
+echo "Which chapter?"
+read chap
+
+echo "Enter number of directories:"
+read num
+
+ones=1
+tens=0
+c=1
+
+while [ "$tens$ones" -le "$num" ];
+do
+	if test -e "$tens$ones"; 
+	then
+		echo "$tens$ones/ exists"
+		cd "$tens$ones"
+	else
+		echo "$tens$ones/ does not exist"
+		mkdir "$tens$ones"
+		cd "$tens$ones"
+	fi
+
+	if test -e "README.md";
+	then
+		echo "$tens$ones/README.md exists"
+		cd ../
+	else
+		echo "$tens$ones/README.md does not exist"
+		if [ $n -eq 0 ];
+		then
+			echo "## Exercise $chap.$tens$ones
+
+
+
+## Solution" >> README.md
+			cd ../
+		else
+			echo "## Project $chap.$tens$ones
+
+
+
+			## Solution" >> README.md
+			echo "#include <stdio.h>
+
+			int main(void)
+			{
+				
+
+				return 0;
+			}" >> $c.c
+			cd ../
+		fi
+	fi
+	
+	c=$((c+1))
+	ones=$((ones+1))
+	if [ $ones -eq 10 ]; 
+	then
+		ones=0
+		tens=$((tens+1))
+	fi
+done
+echo "done :)"
